ce9c10549cb16c4d357d7a0cecbdfaf9b17f9987
MMavrovv <122445997+MMavrovv@users.noreply.github.com>
2023-01-11 12:43:40 +0200

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,300 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "300", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#ebebeb",
+    state_hover_color: "#19538d",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#d5269a",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    BGR2231: {
+      name: "Sliven",
+      description: "Сливен"
+    },
+    BGR2232: {
+      name: "Haskovo",
+      description: "Хасково"
+    },
+    BGR2233: {
+      name: "Stara Zagora",
+      description: "Стара Загора"
+    },
+    BGR2234: {
+      name: "Pazardzhik",
+      description: "Пазарджик"
+    },
+    BGR2235: {
+      name: "Plovdiv",
+      description: "Пловдив"
+    },
+    BGR2236: {
+      name: "Smolyan",
+      description: "Смолян"
+    },
+    BGR2237: {
+      name: "Kardzhali",
+      description: "Кърджали"
+    },
+    BGR2243: {
+      name: "Grad Sofiya",
+      description: "Град София"
+    },
+    BGR2244: {
+      name: "Sofia",
+      description: "София"
+    },
+    BGR2245: {
+      name: "Pernik",
+      description: "Перник"
+    },
+    BGR2246: {
+      name: "Gabrovo",
+      description: "Габрово"
+    },
+    BGR2247: {
+      name: "Lovech",
+      description: "Ловеч"
+    },
+    BGR2248: {
+      name: "Pleven",
+      description: "Плевен"
+    },
+    BGR2249: {
+      name: "Veliko Tarnovo",
+      description: "Велико Търново"
+    },
+    BGR2250: {
+      name: "Montana",
+      description: "Монтана"
+    },
+    BGR2251: {
+      name: "Vratsa",
+      description: "Враца"
+    },
+    BGR2252: {
+      name: "Kyustendil",
+      description: "Кюстендил"
+    },
+    BGR2253: {
+      name: "Vidin",
+      description: "Видин"
+    },
+    BGR2254: {
+      name: "Burgas",
+      description: "Бургас"
+    },
+    BGR2255: {
+      name: "Yambol",
+      description: "Ямбол"
+    },
+    BGR2256: {
+      name: "Razgrad",
+      description: "Разград"
+    },
+    BGR2257: {
+      name: "Targovishte",
+      description: "Търговище"
+    },
+    BGR2258: {
+      name: "Shumen",
+      description: "Шумен"
+    },
+    BGR2259: {
+      name: "Dobrich",
+      description: "Добрич"
+    },
+    BGR2260: {
+      name: "Varna",
+      description: "Варна"
+    },
+    BGR2261: {
+      name: "Silistra",
+      description: "Силистра"
+    },
+    BGR2262: {
+      name: "Ruse",
+      description: "Русе"
+    },
+    BGR3002: {
+      name: "Blagoevgrad",
+      description: "Благоевград"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "42.683333",
+      lng: "23.316667",
+      name: "FELS Debate Club",
+      color: "#2a8aea",
+      description: "тел. 0887 422 507, Велина Андонова"
+    },
+    "1": {
+      lat: "42.7",
+      lng: "23.316667",
+      name: "ACS Debate Club",
+      color: "#2a8aea",
+      description: "Мария Мачева, 0883522836, acsdebateclub@gmail.com"
+    },
+    "2": {
+      lat: "43.4923",
+      lng: "25.571443",
+      name: "MG Baba Tonka",
+      description: "тел. 0895806033, Александра Хачерян",
+      color: "#2a8aea"
+    },
+    "3": {
+      lat: 42.189,
+      lng: 24.333,
+      name: "Debate Club Venchev",
+      color: "#2a8aea",
+      description: "Латинка Минковa, 0895709887, latinkaminkova717@gmail.com"
+    },
+    "4": {
+      lat: "42.622",
+      lng: "23.424",
+      name: "2АЕГ \"Томас Джеферсън\" и ЦИКО филиал Белите брези",
+      color: "#2a8aea",
+      description: "Ния Балчева\t0894450979\tn.balcheva5@gmail.com"
+    },
+    "5": {
+      lat: 42.423,
+      lng: 25.635,
+      name: "ГПЧЕ Ромен Ролан",
+      description: "Георги Тодоров, Анна-Мария Богданова, Йоан Иванов, 0888817692 - Анна-Мария, rrdebateclub22@gmail.com",
+      color: "#2a8aea"
+    },
+    "6": {
+      lat: 43.206,
+      lng: 27.925,
+      name: "МГ \"Д-р Петър Берон\", Варна",
+      color: "#2a8aea",
+      description: "Екатерина Веселинова \t0884099032\tekaterina_veselinova@abv.bg"
+    },
+    "7": {
+      lat: 42.697,
+      lng: 23.324,
+      name: "97 СУ \"Братя Миладинови\"",
+      color: "#2a8aea",
+      description: "Иван Шопов\t+359887872549\tiv.vshopov@gmail.com"
+    },
+    "8": {
+      lat: 43.408,
+      lng: 24.621,
+      name: "Център за работа с деца (ЦПЛР)",
+      color: "#2a8aea",
+      description: "Жанета Спасова\t0886465699\tjaneta.spasova@abv.bg"
+    },
+    "9": {
+      lat: "42.644",
+      lng: 23.324,
+      name: "СГСАГ \"Христо Ботев\"",
+      color: "#2a8aea",
+      description: "Людмила Карамитева\t0887027578\tkaramitevaludmila@gmail.com"
+    },
+    "10": {
+      lat: "42.655",
+      lng: 23.324,
+      name: "Професионална гимназия по електротехника и автоматика (ПГЕА)",
+      color: "#2a8aea",
+      description: "Христо Д. Малчев\t0888463662\thristodimitrovmalchev@gmail.com"
+    },
+    "11": {
+      lat: "42.383",
+      lng: 25.635,
+      name: "Център за подкрепа за личностно развитие (ЦПЛР Стара Загора)",
+      color: "#2a8aea",
+      description: "Станка Димитрова Стойчева\t0888592503\tstanka_stoicheva@abv.bg"
+    },
+    "12": {
+      lat: 41.527,
+      lng: 25.41,
+      name: "Духовно училище ",
+      color: "#2a8aea",
+      description: "Фикрие Бозова \t894467108\tfikriye87@abv.bg"
+    },
+    "13": {
+      lat: "42.400",
+      lng: 27.472,
+      name: "ППМГ \"Акад. Н. Обрешков\"",
+      color: "#2a8aea",
+      description: "Дарина Илиева, 0884643183"
+    },
+    "14": {
+      lat: "42.300",
+      lng: 27.472,
+      name: "ПГИТ (ПГ по компютърно програмиране и иновации)",
+      color: "#2a8aea",
+      description: "г-жа Димитрина Тодорова, директор, +359 877 086 699, dtodorova@codingburgas.bg,"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
